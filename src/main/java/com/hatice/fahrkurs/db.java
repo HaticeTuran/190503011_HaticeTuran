0c6445b61c2c48326b078e9fefa69acec905222b
Hatice <e190503011@stud.tau.edu.tr>
2022-04-26 00:47:25 +0300

    Database baglantisi yapildi 2

---
+++ src/main/java/com/hatice/fahrkurs/db.java
@@ -0,0 +1,27 @@
+package com.hatice.fahrkurs;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Set;
+
+public class db {
+    public  static Connection conn = null;
+
+    public void connect() {
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:\\Users\\Acer\\IdeaProjects\\FahrKurs\\src\\main\\java\\com\\hatice\\fahrkurs\\FahrKurs.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO Person VALUES('Hatice')");
+            ResultSet res = stmt.executeQuery("SELECT * FROM Person");
+            if(res.next()){
+                System.out.println("username: "+res.getString("Name"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
