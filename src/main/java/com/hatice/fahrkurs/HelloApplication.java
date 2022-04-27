d0429a8f4559f4dbfb511f8f82247f2d845bca1f
Hatice <e190503011@stud.tau.edu.tr>
2022-04-27 17:56:38 +0300

    Scene switch: first try

--- src/main/java/com/hatice/fahrkurs/HelloApplication.java
+++ src/main/java/com/hatice/fahrkurs/HelloApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+/*public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -18,21 +18,21 @@ public class HelloApplication extends Application {
         db database = new db();
         database.connect();
 
-    }
+    }*/
 
-/*public class HelloApplication extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Home");
         stage.setScene(scene);
         stage.show();
-        db database = new db();
-        database.connect();
+        //db database = new db();
+        //database.connect();
 
     }
-*/
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/hatice/fahrkurs/HomeController.java
+++ src/main/java/com/hatice/fahrkurs/HomeController.java
@@ -1,24 +1,46 @@
 package com.hatice.fahrkurs;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.EventObject;
+import java.util.Objects;
 
 public class HomeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private Button Anmeldung;
+
+
     /*@FXML
     private TextField BenutzerName = new TextField();
 
     @FXML
     private PasswordField Passwort = new PasswordField();
-*/
-    @FXML
-    private Button Anmeldung;
 
     @FXML
-    public void onAnmeldungButtonClick(){
+    private Button Anmeldung;*/
+
+    @FXML
+    public void onAnmeldungButtonClick(ActionEvent event) throws Exception{
+        //root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("page1.fxml")));
+        root = FXMLLoader.load(getClass().getResource("page1.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
         //change the page
+
     }
 
 }
