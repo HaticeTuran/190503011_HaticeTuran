9e61d5d951257647e9c6fcd5251caf44d5d82b65
Hatice <e190503011@stud.tau.edu.tr>
2022-05-01 03:59:37 +0300

    Aktion class yeni versiyon

---
+++ src/main/java/com/hatice/fahrkurs/Aktion1Controller.java
@@ -0,0 +1,15 @@
+package com.hatice.fahrkurs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Aktion1Controller {
+    @FXML
+    private AnchorPane aP;
+
+    @FXML
+    private TextField SchuelerList;
+
+
+}
