1eb06caa9902643c4252c9a7d60f45412e376603
Hatice <e190503011@stud.tau.edu.tr>
2022-05-11 11:51:52 +0300

    ogrenci silme işlemi başarı ile yapılıyor.

--- src/main/java/com/hatice/fahrkurs/Aktion1Controller.java
+++ src/main/java/com/hatice/fahrkurs/Aktion1Controller.java
@@ -4,12 +4,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class Aktion1Controller {
+public class Aktion1Controller implements Initializable {
     @FXML
     private AnchorPane aP;
 
@@ -27,6 +30,11 @@ public class Aktion1Controller {
 
     @FXML
     private Button show;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        this.List();
+    }
 
     public void List( ){
         ObservableList arr = db.listSchueler();
