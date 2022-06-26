e28b83a38c3a6dd4896e49faab2d1b2067ede9ef
Hatice <e190503011@stud.tau.edu.tr>
2022-06-26 22:42:35 +0300

    Persaonal silme işlemi yapılıyor.

--- src/main/java/com/hatice/fahrkurs/Operation3Controller.java
+++ src/main/java/com/hatice/fahrkurs/Operation3Controller.java
@@ -1,4 +1,45 @@
 package com.hatice.fahrkurs;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
 public class Operation3Controller {
+
+    @FXML
+    private Button Delete;
+
+    @FXML
+    private Label ErrMsg;
+
+    @FXML
+    private ChoiceBox<?> Personal;
+
+    private Personal[] PersonalList = db.getPersonalList().toArray(new Personal[db.getPersonalList().size()]);
+
+    @FXML
+    public void initialize() {
+        ObservableList a = FXCollections.observableArrayList();
+
+        for(Personal p : PersonalList){
+            a.add(p);
+        }
+        Personal.getItems().addAll(a);
+
+    }
+
+    @FXML
+    void DeletePersonal(ActionEvent event) {
+        String act = (String) this.Personal.getSelectionModel().getSelectedItem().toString();
+        String[] arrOfStr = act.split(" ");
+
+        System.out.println(arrOfStr[6]);
+
+        db.deletePersonal(arrOfStr[6]);
+    }
+
 }
